1c3db896691bca03d37048312ee67683bd0daf4e
纪灿钦 <20203349@std.neu.edu.cn>
2022-12-14 16:03:07 +0800

    #3.0.0 串口实验

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,17 @@
-/*2.按键输入，GPIO输入，中断方式*/
+/*3.串口实验*/
 #include <Arduino.h>
 
-#define LED_GPIO_PIN 2
-#define KEY_GPIO_PIN 0
-
-bool keyStatus;
-
-void keyCallback(); // 中断回调函数声明
 void setup()
 {
-	pinMode(LED_GPIO_PIN, OUTPUT);						// LED输出模式
-	pinMode(KEY_GPIO_PIN, INPUT);						// 输入上拉模式
-	attachInterrupt(KEY_GPIO_PIN, keyCallback, CHANGE); // 中断使能
+	// put your setup code here, to run once:
+	Serial.begin(115200); // 将UART0的波特率初始化为115200
 }
 
+int recData;
 void loop()
 {
-}
-
-void keyCallback()
-{
-	// 读取按键状态
-	keyStatus = digitalRead(KEY_GPIO_PIN);
-	// 把按键状态作为LED的输出
-	digitalWrite(LED_GPIO_PIN, !keyStatus);
+	// put your main code here, to run repeatedly:
+	Serial.print("纪灿钦！");
+	Serial.println("This is a uart0 demo!"); // 打印并换行
+	delay(1000);
 }
