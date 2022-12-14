6a55c3655d237b7bd3e61cbb4eaec0076da6977e
纪灿钦 <20203349@std.neu.edu.cn>
2022-12-14 15:53:54 +0800

    #2.0.0 按键输入，GPIO输入实验

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,22 @@
-/*1.点亮LED，GPIO输出*/
+/*1.按键，GPIO输入*/
 #include <Arduino.h>
 
 #define LED_GPIO_PIN 2
+#define KEY_GPIO_PIN 0
+
+bool keyStatus;
 
 void setup()
 {
 	// put your setup code here, to run once:
 	pinMode(LED_GPIO_PIN, OUTPUT); // LED输出模式
+	pinMode(KEY_GPIO_PIN, INPUT);  // 输入上拉模式
 }
 
 void loop()
 {
+	// 读取按键状态
+	keyStatus = digitalRead(KEY_GPIO_PIN);
 	// 把按键状态作为LED的输出
-	digitalWrite(LED_GPIO_PIN, HIGH);
-	delay(500);
-	digitalWrite(LED_GPIO_PIN, LOW);
-	delay(500);
+	digitalWrite(LED_GPIO_PIN, !keyStatus);
 }
