38b9ec2ad2c727ddcf5513e47e78cf6c1a8a3bf7
纪灿钦 <20203349@std.neu.edu.cn>
2022-12-14 15:56:36 +0800

    #2.1.0 按键输入，GPIO输入，中断方式

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-/*1.按键，GPIO输入*/
+/*2.按键输入，GPIO输入，中断方式*/
 #include <Arduino.h>
 
 #define LED_GPIO_PIN 2
@@ -6,14 +6,19 @@
 
 bool keyStatus;
 
+void keyCallback(); // 中断回调函数声明
 void setup()
 {
-	// put your setup code here, to run once:
-	pinMode(LED_GPIO_PIN, OUTPUT); // LED输出模式
-	pinMode(KEY_GPIO_PIN, INPUT);  // 输入上拉模式
+	pinMode(LED_GPIO_PIN, OUTPUT);						// LED输出模式
+	pinMode(KEY_GPIO_PIN, INPUT);						// 输入上拉模式
+	attachInterrupt(KEY_GPIO_PIN, keyCallback, CHANGE); // 中断使能
 }
 
 void loop()
+{
+}
+
+void keyCallback()
 {
 	// 读取按键状态
 	keyStatus = digitalRead(KEY_GPIO_PIN);
