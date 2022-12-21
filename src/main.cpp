42a98e541922e1a09ae3e4d54b1cd05490622aec
纪灿钦 <20203349@std.neu.edu.cn>
2022-12-21 18:12:51 +0800

    #8.0.0 双核实验

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,63 @@
-/*7.EEPROM实验*/
+/*8.双核实验*/
 #include <Arduino.h>
-#include <EEPROM.h>
+
+TaskHandle_t Task1;
+TaskHandle_t Task2;
+
+// LED pins
+const int led1 = 2;
+void Task1code(void *pvParameters);
+void Task2code(void *pvParameters);
 
 void setup()
 {
 	Serial.begin(115200);
-	Serial.println("");
+	pinMode(led1, OUTPUT);
 
-	EEPROM.begin(4096); // 申请4096个字节的EEPROM存储空间
-	// 写数据
-	Serial.println("write begin");
-	for (int addr = 0; addr < 4096; addr++)
+	// create a task that will be executed in the Task1code() function, with priority 1 and executed on core 0
+	xTaskCreatePinnedToCore(
+		Task1code, /* Task function. */
+		"Task1",   /* name of task. */
+		10000,	   /* Stack size of task */
+		NULL,	   /* parameter of the task */
+		1,		   /* priority of the task */
+		&Task1,	   /* Task handle to keep track of created task */
+		0);		   /* pin task to core 0 */
+	delay(500);
+
+	// create a task that will be executed in the Task2code() function, with priority 1 and executed on core 1
+	xTaskCreatePinnedToCore(
+		Task2code, /* Task function. */
+		"Task2",   /* name of task. */
+		10000,	   /* Stack size of task */
+		NULL,	   /* parameter of the task */
+		1,		   /* priority of the task */
+		&Task2,	   /* Task handle to keep track of created task */
+		1);		   /* pin task to core 1 */
+	delay(500);
+}
+
+// Task1code: blinks an LED every 1000 ms
+void Task1code(void *pvParameters)
+{
+	Serial.print("Task1 running on core ");
+	Serial.println(xPortGetCoreID());
+
+	for (;;)
 	{
-		int data = addr % 256;	  // EEPROM写入的数据时以字节为单位的
-		EEPROM.write(addr, data); // 写入数据
+		digitalWrite(led1, HIGH);
+		delay(1000);
+		digitalWrite(led1, LOW);
+		delay(1000);
 	}
-	EEPROM.commit(); // 保存更改的数据
-	Serial.println("write done");
+}
 
-	// 读数据
-	Serial.println("read begin");
-	for (int addr = 0; addr < 4096; addr++)
-	{
-		int data = EEPROM.read(addr); // 读数据
-		Serial.print(data);
-		Serial.print(" ");
-		delay(2);
-		if ((addr + 1) % 256 == 0) // 每行显示256个数据
-		{
-			Serial.println("");
-		}
-	}
-
-	Serial.println("read done");
+// Task2code: blinks an LED every 500 ms
+void Task2code(void *pvParameters)
+{
+	Serial.print("Task2 running on core ");
+	Serial.println(xPortGetCoreID());
+	delay(1000);
 }
 
 void loop()
